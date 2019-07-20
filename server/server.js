6831132a3cd6b697735da115e27e88d54c65b43f
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-07-20 09:43:14 -0500

    proxy deployment with docker/elasticbeanstalk

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(express.static('./public'))
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/ProductOverview', (req, res) => {
     res.send('../../ProductOverview/public/index.html')})
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
           plugins: ['@babel/proposal-class-properties']
         }
        }
-     }
+     },
    ]
 }
 };
